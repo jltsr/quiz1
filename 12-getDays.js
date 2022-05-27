a7c839d382d31ccb4fe9c1d19d89dd47ea0f4206
jltsr <julitasari151@gmail.com>
2022-05-27 12:15:11 +0700

    first commit

--- 12-getDays.js
+++ 12-getDays.js
@@ -9,81 +9,86 @@ function getDays(month,year){
         return 'inputan tahun harus dalam integer'
     }else{
         if(year % 4 === 0){
-            if(month===1){
-                return 'This month has 31 days, 2000 adalah tahun kabisat'
-
-            }else if(month===2){
-                return 'This month has 29 days, 2000 adalah tahun kabisat'
-
-            }else if(month===3){
-                return 'This month has 31 days, 2000 adalah tahun kabisat'
-
-            }else if(month===4){
-                return 'This month has 30 days, 2000 adalah tahun kabisat'
-
-            }else if(month===5){
-                return 'This month has 31 days, 2000 adalah tahun kabisat'
-
-            }else if(month===6){
-                return 'This month has 30 days, 2000 adalah tahun kabisat'
-
-            }else if(month===7){
-                return 'This month has 31 days, 2000 adalah tahun kabisat'
-
-            }else if(month===8){
-                return 'This month has 31 days, 2000 adalah tahun kabisat'
-
-            }else if(month===9){
-                return 'This month has 30 days, 2000 adalah tahun kabisat'
-
-            }else if(month===10){
-                return 'This month has 31 days, 2000 adalah tahun kabisat'
-
-            }else if(month===11){
-                return 'This month has 30 days, 2000 adalah tahun kabisat'
-
-            }else if(month===12){
-                return 'This month has 31 days, 2000 adalah tahun kabisat'
-
+            switch(month){
+                case 1: 
+                    return  'This month has 31 days,' +year+ ' adalah tahun kabisat';
+                    break;
+                case 2: 
+                    return  'This month has 29 days,' +year+ ' adalah tahun kabisat';
+                    break;
+                case 3: 
+                    return  'This month has 31 days, ' +year+ ' adalah tahun kabisat';
+                    break;
+                case 4: 
+                    return  'This month has 30 days, ' +year+ ' adalah tahun kabisat';
+                    break;
+                case 5: 
+                    return  'This month has 31 days, ' +year+ ' adalah tahun kabisat';
+                    break;
+                case 6: 
+                    return  'This month has 30 days, ' +year+ ' adalah tahun kabisat';
+                    break;
+                case 7: 
+                    return  'This month has 31 days, ' +year+ ' adalah tahun kabisat';
+                    break;
+                case 8: 
+                    return  'This month has 31 days, ' +year+ ' adalah tahun kabisat';
+                    break;
+                case 9: 
+                    return  'This month has 30 days, ' +year+ ' adalah tahun kabisat';
+                    break;
+                case 10: 
+                    return  'This month has 31 days, ' +year+ ' adalah tahun kabisat';
+                    break;
+                case 11: 
+                    return  'This month has 30 days, ' +year+ ' adalah tahun kabisat';
+                    break;
+                case 12: 
+                    return  'This month has 31 days, ' +year+ ' adalah tahun kabisat';
+                    break;
+                
+        
             }
-
         }else{
-            if(month===1){
-                return 'This month has 31 hari'
-
-            }else if(month===2){
-                return 'This month has 29 hari'
-
-            }else if(month===3){
-                return 'This month has 31 hari'
-
-            }else if(month===4){
-                return 'This month has 30 hari'
-
-            }else if(month===5){
-                return 'This month has 31 hari'
-
-            }else if(month===6){
-                return 'This month has 30 hari'
-
-            }else if(month===7){
-                return 'This month has 31 hari'
-
-            }else if(month===8){
-                return 'This month has 31 hari'
-
-            }else if(month===9){
-                return 'This month has 30 hari'
-
-            }else if(month===10){
-                return 'This month has 31 hari'
-
-            }else if(month===11){
-                return 'This month has 30 hari,'
-
-            }else if(month===12){
-                return 'This month has 31 hari'
-
+            switch(month){
+                case 1: 
+                    return  'This month has 31 hari';
+                    break;
+                case 2: 
+                    return  'This month has 28 hari';
+                    break;
+                case 3: 
+                    return  'This month has 31 hari';
+                    break;
+                case 4: 
+                    return  'This month has 30 hari';
+                    break;
+                case 5: 
+                    return  'This month has 31 hari';
+                    break;
+                case 6: 
+                    return  'This month has 30 hari';
+                    break;
+                case 7: 
+                    return  'This month has 31 hari';
+                    break;
+                case 8: 
+                    return  'This month has 31 hari';
+                    break;
+                case 9: 
+                    return  'This month has 30 hari';
+                    break;
+                case 10: 
+                    return  'This month has 31 hari';
+                    break;
+                case 11: 
+                    return  'This month has 30 hari';
+                    break;
+                case 12: 
+                    return  'This month has 31 hari';
+                    break;
+                
+        
             }
 
         }
@@ -94,5 +99,5 @@ function getDays(month,year){
 console.log(getDays("a",2021)); //inputan bulan harus dalam integer
 console.log(getDays("2","year")); //inputan tahun harus dalam integer
 console.log(getDays("m","year"));//inputan bulan & tahun harus dalam integer
-console.log(getDays(2,2000)); //This month has 29 days, 2000 adalah tahun kabisat
+console.log(getDays(1,2000)); //This month has 29 days, 2000 adalah tahun kabisat
 console.log(getDays(8,2021)); //This month has 31 hari
